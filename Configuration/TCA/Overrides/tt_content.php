26c3b3f4eab47b7430047e685cc7457b3ed00172
Julian Hofmann <j.hofmann@netzhaut.de>
2017-05-04 14:33:11 +0200

    [Bugfix] Limit number of images (Currently this extension supports only up to 8 images in a block)

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -16,6 +16,7 @@ call_user_func(function () {
     // use the $GLOBALS['TCA'] definition from textpic
     $GLOBALS['TCA']['tt_content']['types']['magstyleimages_images']['showitem'] = $GLOBALS['TCA']['tt_content']['types']['textpic']['showitem'];
     $GLOBALS['TCA']['tt_content']['types']['magstyleimages_images']['columnsOverrides'] = $GLOBALS['TCA']['tt_content']['types']['textpic']['columnsOverrides'];
+    $GLOBALS['TCA']['tt_content']['types']['magstyleimages_images']['columnsOverrides']['image']['config']['maxitems'] = 8;
 
     if (TYPO3_MODE == 'BE') {
         $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses'][\Webenergy\Magstyleimages\Wizicon::class] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('magstyleimages') . 'Classes/Wizicon.php';
