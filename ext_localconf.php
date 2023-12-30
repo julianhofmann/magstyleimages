3b582787be4e91e37d7ddca92c3e3401b60545b1
Julian Hofmann <info@julian-hofmann.net>
2023-12-30 21:08:27 +0100

    [TASK] Move PageTS inclusion
    Starting with TYPO3 version 12.0 page TSconfig from Configuration/page.tsconfig is automatically included for all pages.

--- ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-ExtensionManagementUtility::addPageTSConfig(
-    "@import 'EXT:magstyleimages/Configuration/TsConfig/Page/Mod/Wizards/MagstyleimagesImages.tsconfig'"
-);
